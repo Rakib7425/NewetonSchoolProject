bb318a31faefe125a55140358c75677c050d17a6
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-06-16 22:55:00 +0530

    modified:   Maths_Partner/scripts/index.js

--- Maths_Partner/scripts/index.js
+++ Maths_Partner/scripts/index.js
@@ -6,7 +6,6 @@ const fetchCall = async () => {
     const problem = document.getElementById('problem').value;
     const category = document.getElementById('category').value;
     const url = `https://newton.vercel.app/api/v2/${category}/${problem}`
-    // console.log(url);
 
     const response = await fetch(url);
     const jsonData = await response.json();
@@ -14,21 +13,17 @@ const fetchCall = async () => {
     data.push(jsonData);
     // console.log(data[0].result);
     // console.log(category.value, data.result);
+    const fLetterCaps = category.charAt(0).toUpperCase() + category.slice(1)
     answer.innerHTML = `
         <div class="ans" id="ans">
-            <h3 class="mt-10">${category} : ${problem} </h3>
+            <h3 class="mt-10">${fLetterCaps} : ${problem} </h3>
 
             <div class="finalAns" id="finalAns">
-                <h4 class="f" id="f">Final Answer &nbsp; &nbsp; <b> ${data[0].result} </b></h4>
+                <h4 class="f" id="f">Final Answer &nbsp; : &nbsp; <b> ${data[0].result} </b></h4>
             </div>
             <div class="delete">
                 <button class="delete-btn" id="delete" role="button">Delete</button>
             </div>
         </div>
     `
-
-    // for (let i = 0; i < data.length; i++) {
-    //     data.pop();
-    // }
-    // console.log(data);
 }
