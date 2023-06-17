f99457bd67fb3486085db24aea8a335b438805ee
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-06-17 12:48:18 +0530

    modified:   Maths_Partner/index.html 	modified:   Maths_Partner/scripts/index.js 	modified:   Maths_Partner/styles/style.css

--- Maths_Partner/scripts/index.js
+++ Maths_Partner/scripts/index.js
@@ -1,12 +1,15 @@
 const answer = document.getElementById('answer');
 
-const data = [];
+let data = [];
 
+if (localStorage.getItem("problems")) {
+    data = JSON.parse(localStorage.getItem("problems"));
+}
 
 const fetchCall = async () => {
+
     const problem = document.getElementById('problem').value;
     const category = document.getElementById('category').value;
-
     const url = `https://newton.vercel.app/api/v2/${category}/${problem}`
 
     const response = await fetch(url);
@@ -17,19 +20,41 @@ const fetchCall = async () => {
     // console.log(data[0].result);
     // console.log(category.value, data.result);
     const fLetterCaps = category.charAt(0).toUpperCase() + category.slice(1);
-
-    answer.innerHTML = `
+    for (var i = 0; i < data.length; i++) {
+        answer.innerHTML = `
         <div class="ans" id="ans">
             <h3 class="mt-10">${fLetterCaps} : ${problem} </h3>
-
             <div class="finalAns" id="finalAns">
-                <h4 class="f" id="f">Final Answer &nbsp; : &nbsp; <b> ${data[0].result} </b></h4>
+                <h4 class="f" id="f">Final Answer &nbsp; : &nbsp; <b> ${data[i].result} </b></h4>
             </div>
-            <div class="delete">
-                <button class="delete-btn" id="delete" role="button">Delete</button>
-            </div>
+            
         </div>
     `
+    }
 }
 
 
+for (var i = 0; i < data.length; i++) {
+    var card = document.createElement("div");
+    card.classList.add("problem-card");
+    card.innerHTML = `
+        <div class="cards"> 
+            <p>Category: ${data[i].operation}</p>
+            <h3>Problem: ${data[i].expression}</h3>
+            <p>Solution: ${data[i].result}</p>
+            <button class="deleteBtn delete-btn" data-index="${i}">Delete</button>
+        </div>
+    `;
+    document.getElementById('historyCardsContainer').appendChild(card);
+}
+
+
+var deleteButtons = document.getElementsByClassName("deleteBtn");
+for (var i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].addEventListener("click", function () {
+        var index = this.getAttribute("data-index");
+        data.splice(index, 1);
+        localStorage.setItem("problems", JSON.stringify(data));
+        this.parentNode.remove();
+    });
+}
