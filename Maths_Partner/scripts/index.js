c45b9d9e69ab82123b5b02fcbe4cb7de98bd347c
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-06-19 14:35:50 +0530

    modified:   Maths_Partner/index.html 	modified:   Maths_Partner/scripts/index.js 	modified:   Maths_Partner/scripts/savedscript.js 	modified:   Maths_Partner/src/saved.html 	modified:   Maths_Partner/styles/style.css

--- Maths_Partner/scripts/index.js
+++ Maths_Partner/scripts/index.js
@@ -2,15 +2,21 @@ const answer = document.getElementById('answer');
 
 let data = [];
 
+
 if (localStorage.getItem("problems")) {
     data = JSON.parse(localStorage.getItem("problems"));
 }
 
+
+/**
+ *  API call based on user input, stores it in local storage, and displays
+ * the result on the webpage.
+ */
 const fetchCall = async () => {
 
     const problem = document.getElementById('problem').value;
     const category = document.getElementById('category').value;
-    const url = `https://newton.vercel.app/api/v2/${category}/${encodeURIComponent(problem)}`
+    const url = `https://newton.vercel.app/api/v2/${category}/${myURIfn(problem)}`
 
     const response = await fetch(url);
     const jsonData = await response.json();
@@ -25,11 +31,19 @@ const fetchCall = async () => {
         <div class="ans" id="ans">
             <h2 class="mt-10">${fLetterCaps} : ${problem} </h2>
             <div class="finalAns" id="finalAns">
-                <h2 class="f" id="f">Final Answer : &nbsp; <b> ${data[i].result} </b></h2>
+                <h2 class="f" id="f">Result: &nbsp; <b> ${data[i].result} </b></h2>
             </div>
         </div>
     `
     }
 
 }
+// replace of encodeURIComponent functionality in js engine
 
+function myURIfn(str) {
+    const reservedChars = /[!'()*]/g;
+
+    return str.replace(reservedChars, (match) => {
+        return '%' + match.charCodeAt(0).toString(16).toUpperCase();
+    });
+}
--- Maths_Partner/scripts/savedscript.js
+++ Maths_Partner/scripts/savedscript.js
@@ -1,25 +1,36 @@
+
 let data = [];
 
 if (localStorage.getItem("problems")) {
     data = JSON.parse(localStorage.getItem("problems"));
 }
 
+/* checking if there is any data stored in the `data` array. If there is data, it
+creates a card for each item in the array and displays it on the DOM.
+*/
+
 if (data.length > 0) {
-    //  Create Cards and Show to the Dom
+
+    /* Create Cards and Show to the Dom */
+
     for (let i = 0; i < data.length; i++) {
         let card = document.createElement("div");
         card.classList.add("problem-card");
         card.innerHTML = `
         <div class="cards"> 
-            <h3>Category: ${data[i].operation}</h3>
-            <h3>Problem: ${data[i].expression}</h3>
-            <h2>Solution: ${data[i].result}</h2>
+            <h5>Category: ${data[i].operation}</h5>
+            <h5>Problem: ${data[i].expression}</h5>
+            <h4>Solution: ${data[i].result}</h4>
             <button class="deleteBtn delete-btn" data-index="${i}">Delete</button>
         </div>
     `;
         document.getElementById('historyCardsContainer').appendChild(card);
     }
 
+    /* selecting all the elements with the class name "deleteBtn" and assigning them to the `deleteButtons` variable.
+
+    Finally,
+    delete a specific item from the data array and the local storage by clicking on the delete button associated with that item. */
     let deleteButtons = document.getElementsByClassName("deleteBtn");
     for (let i = 0; i < deleteButtons.length; i++) {
         deleteButtons[i].addEventListener("click", function () {
@@ -30,6 +41,10 @@ if (data.length > 0) {
         });
     }
 }
+
+/*  It creates a new `h1`
+"No Data Found" */
+
 else {
     let h1 = document.createElement("h1");
     h1.innerHTML = `<h1> No Data Found </h1>`;
